62e970f01a93ab43bdf9dcb2608bb946e10392b9
arek600r <arkadiusz_krajewski@tutanota.com>
2025-06-30 17:01:58 +0200

    Zmiany

--- Day1/Cw1.py
+++ Day1/Cw1.py
@@ -1,14 +1,32 @@
 import os
 import sys
+from pathlib import Path
+import time
 
 if len(sys.argv) != 2:
     sys.exit("usage: args.py <path>")
 
-path = sys.argv[1]
-print(f"Path: {path}")
+main_path = sys.argv[1]
+print(f"Path: {main_path}")
 
-for path, dirs, files in os.walk(path):
-    for file in files:
-        _, ext = os.path.splitext(file)
-        ext.lower()
-        print(f"{path}/{file}")
+print("Method1")
+for path, dirs, files in os.walk(main_path):
+  path_elements = path.split(os.path.sep)
+    
+  if any([x.startswith(".") for x in path_elements]):
+    print(path_elements)
+  for file in files:
+      #print(file)
+      #print(file_elements)
+      #
+      #    continue
+      _, ext = os.path.splitext(file)
+      ext.lower()
+      #print(ext)
+      #print(f"{path}/{file}")
+
+print("------------")
+time.sleep(1.5)
+#print("Method2")
+#for i in Path(main_path).rglob("*"):
+#    print(i)
